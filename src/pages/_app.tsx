05362a7f3a7d1d7b22bc7190354ed5ec3b3c6958
Peter Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2022-08-26 20:52:51 -0400

    Move Cloudflare analytics script to _app.tsx (#2)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Layout from "../components/library/Layout";
 import { CSSReset, GlobalStyles } from "../styles/globals";
 
@@ -9,6 +10,10 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Layout>
         <Component {...pageProps} />
+        <Script
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "659601c3710f4a01aacef8ae1f39132a"}'
+        />
       </Layout>
     </>
   );
